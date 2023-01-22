d5c5b11d1fba3c14c43f38502817bf4735360329
vernaloqui <vernadetteloquinario@gmail.com>
2023-01-22 20:18:14 +0800

    added manifest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar>
-        
+        <Routes>
+          <Route path="/" element={<About/>}/>
+          <Route path="/proj" element={<Projects/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
       </Navbar>
+      </BrowserRouter>
     </div>
   );
 }
